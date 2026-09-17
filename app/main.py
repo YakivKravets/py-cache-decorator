5620e4cc397fd91cc7f925cf2989a2677ed0dec5
Yakiv Kravets <fghfctv@gmail.com>
2026-09-17 19:14:14 +0300

    done all

--- app/main.py
+++ app/main.py
@@ -2,5 +2,17 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    results: dict[tuple, object] = {}
+
+    def wrapper(*args: object) -> object:
+        if args in results:
+            print("Getting from cache")
+            return results[args]
+
+        print("Calculating new result")
+        result = func(*args)
+        results[args] = result
+
+        return result
+
+    return wrapper
